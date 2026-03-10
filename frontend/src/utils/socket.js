0eec9b214c385df2a0e66436f6b1c8e1e7f93443
Aljas Muthukadan <aljasmuthukadan@gmail.com>
2026-03-11 00:31:12 +0530

    Change socket connection URL to production

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
-const socket = io("http://localhost:3000",{
+const socket = io("https://share-sprint.onrender.com",{
     autoConnect: true,
     transports: ["websocket"],
 });
